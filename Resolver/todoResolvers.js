d7f87683a49defc0fd6c48077bc95e7acdd5a169
gauravb2503 <gaurav.b@simformsolutions.com>
2024-11-07 14:25:22 +0530

    Added user in the TODO schemea

--- Resolver/todoResolvers.js
+++ Resolver/todoResolvers.js
@@ -1,11 +1,28 @@
 const axios = require('axios');
+
+const Todo = {
+    user: async (parent) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`);
+        return response.data;
+    },
+};
+
 const Query = {
-    todos: async () => {
+    getTodos: async () => {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        return response.data;
+    },
+    getAllUsers: async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        return response.data;
+    },
+    getUser: async (parent, args) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${args.id}`);
         return response.data;
     },
 };
 
 module.exports = {
     Query,
+    Todo
 };
